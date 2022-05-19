c2fc50e6a1c244dad26d2a16e1eb4415b3ad3503
Braian Molina <braian_gabriel_1993@hotmail.com>
2022-05-19 09:12:18 -0300

    clase 9

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,16 +1,24 @@
 import './TodoSearch.css';
+import React from 'react';
 
 const TodoSearch = () => {
+
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
     return(
-        <input
-            className="TodoSearch"
-            placeholder="Cebolla"
-            onChange={onSearchValueChange}
-        />
+        <>
+            <input
+                className="TodoSearch"
+                placeholder="Cebolla"
+                value={searchValue}
+                onChange={onSearchValueChange}
+            />
+            <p>{searchValue}</p>
+        </>
     );
 };
 
